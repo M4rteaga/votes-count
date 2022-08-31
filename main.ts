32917cac4d516a546dd14baee9c4ecef196d6086
mateo.arteaga <mateo.arteaga@pragma.com.co>
2022-08-31 09:18:48 -0500

    feat: change user score

--- main.ts
+++ main.ts
@@ -13,9 +13,101 @@ const db = fs.getFirestore(firebaseApp);
 
 const router = new Router();
 
-router.get('/checkvotes/:rondaId', async (ctx) => {
+interface FirestoreData {
+	id: string;
+	data: Respuestas;
+}
+
+interface Respuestas {
+	restpuesta: {
+		categoria: string;
+		acceso: string;
+	}[];
+	usuario: string;
+	rondaId: string;
+}
+
+interface Respuesta {
+	palabra: string;
+	categoria: string;
+	votos: number;
+	puntaje: number;
+}
+
+interface Usuarios {
+	user: string;
+	ready: boolean;
+	leader: boolean;
+	score: number;
+}
+
+function reviewVotes(data: FirestoreData[]) {
+	data.forEach(async (r) => {
+		const querySnapchotResponses = await fs.getDocs(
+			fs.query(
+				fs.collection(db, `Respuestas/${r.id}/Respuesta`),
+				fs.where('votos', '<=', 0)
+			)
+		);
+		querySnapchotResponses.docs.forEach(async (e) => {
+			await fs.updateDoc(e.ref, {
+				puntaje: 0,
+			});
+		});
+	});
+}
+
+async function getUserGeneralScore(
+	data: FirestoreData[]
+): Promise<Map<string, number>> {
+	const scoreMap: Promise<Map<string, number>> = new Promise(
+		(resolve, reject) => {
+			try {
+				const userMap = new Map<string, number>();
+				Promise.all(
+					data.map(async (r) => {
+						const user = r.data.usuario;
+						if (!userMap.get(user)) {
+							userMap.set(user, 0);
+						}
+						const querySnapchotResponses = await fs.getDocs(
+							fs.query(fs.collection(db, `Respuestas/${r.id}/Respuesta`))
+						);
+						querySnapchotResponses.docs.forEach((e) => {
+							const resData: Respuesta = e.data();
+							userMap.set(user, userMap.get(user)! + resData.puntaje);
+						});
+					})
+				).then(() => resolve(userMap));
+			} catch (error) {
+				reject(error);
+				throw new Error(error);
+			}
+		}
+	);
+
+	return await scoreMap;
+}
+
+async function updateScoreOfUsers(
+	roomId: string,
+	userScoreMap: Map<string, number>
+) {
+	const querySnapchot = await fs.getDocs(
+		fs.collection(db, `Salas/${roomId}/Usuarios`)
+	);
+	querySnapchot.docs.forEach(async (doc) => {
+		await fs.updateDoc(doc.ref, {
+			score: userScoreMap.get(doc.data().user),
+		});
+	});
+}
+
+router.get('/checkvotes/:roomId/:rondaId', async (ctx) => {
 	try {
 		const rondaId = ctx.params.rondaId;
+		const roomId = ctx.params.roomId;
+
 		const q = fs.query(
 			fs.collection(db, 'Respuestas'),
 			fs.where('rondaId', '==', rondaId)
@@ -26,27 +118,16 @@ router.get('/checkvotes/:rondaId', async (ctx) => {
 			data: doc.data(),
 		}));
 
-		data.forEach(async (r) => {
-			const querySnapchotResponses = await fs.getDocs(
-				fs.query(
-					fs.collection(db, `Respuestas/${r.id}/Respuesta`),
-					fs.where('votos', '<=', 0)
-				)
-			);
-
-			querySnapchotResponses.docs.forEach(async (e) => {
-				await fs.updateDoc(e.ref, {
-					puntaje: 0,
-				});
-			});
-		});
+		await reviewVotes(data);
+		const scoreMap = await getUserGeneralScore(data);
+		await updateScoreOfUsers(roomId!, scoreMap);
 
 		ctx.response.status = 200;
-		ctx.response.body = 'good response';
+		ctx.response.body = 'ok';
 		ctx.response.type = 'text';
 	} catch (error) {
 		ctx.response.status = 404;
-		ctx.response.body = 'no data to update';
+		ctx.response.body = 'something whent wrong';
 		ctx.response.type = 'text';
 		console.log(error);
 	}
